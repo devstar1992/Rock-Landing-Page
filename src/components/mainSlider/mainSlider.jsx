9a542bad6cd2a3f7c8306c05a399674ad732102a
sergei3611 <devstar1992@outlook.com>
2021-08-03 09:57:50 +0900

    Update mainSlider.jsx

--- src/components/mainSlider/mainSlider.jsx
+++ src/components/mainSlider/mainSlider.jsx
@@ -37,7 +37,7 @@ class MainSlider extends Component {
                 // className="col-xs-12 hidden-xs hidden-sm"
               >
                 <Countdown
-                  timeTillDate="08 1 2021, 0:30 am"
+                  timeTillDate="09 5 2021, 9:30 am"
                   timeFormat="MM DD YYYY, h:mm a"
                 />
               </Col>
